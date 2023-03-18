8bdd76944a8809a1426655d33a6ed05ceb0cf6a0
Madh911 <98076468+Madh911@users.noreply.github.com>
2023-03-18 23:53:54 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    char star='*';
+    printf("it is %c \n",star);
+    printf("Hello world \n");
+    printf("Hello world \n");
+    printf("Hello world \n");
+    printf("Hello world \n");
+    return 0;
+}
